dbc2bffa8393dbae72cfacebf4c3231485f46932
Caspern <janedbal@gmail.com>
2015-04-22 16:31:35 +0200

    SmtpMailer: improved exception message on write failure

--- src/Mail/SmtpMailer.php
+++ src/Mail/SmtpMailer.php
@@ -159,7 +159,7 @@ class SmtpMailer extends Nette\Object implements IMailer
 
 
 	/**
-	 * Writes data to server and checks response.
+	 * Writes data to server and checks response against expected code if some provided.
 	 * @param  string
 	 * @param  int   response code
 	 * @param  string  error message
@@ -168,8 +168,11 @@ class SmtpMailer extends Nette\Object implements IMailer
 	protected function write($line, $expectedCode = NULL, $message = NULL)
 	{
 		fwrite($this->connection, $line . Message::EOL);
-		if ($expectedCode && !in_array((int) $this->read(), (array) $expectedCode, TRUE)) {
-			throw new SmtpException('SMTP server did not accept ' . ($message ? $message : $line));
+		if ($expectedCode) {
+			$response = $this->read();
+			if (!in_array((int) $response, (array) $expectedCode, TRUE)) {
+				throw new SmtpException('SMTP server did not accept ' . ($message ? $message : $line) . ' with error: ' . trim($response));
+			}
 		}
 	}
 
