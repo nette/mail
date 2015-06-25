90097d4324891a125ac81c91a4f9a109a27cfc8c
Jan Dvořák <dvorak@spinao.cz>
2015-06-25 23:45:29 +0200

    Repair coding style

---
+++ src/Mail/exceptions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of the Nette Framework (http://nette.org)
+ * Copyright (c) 2004 David Grudl (http://davidgrudl.com)
+ */
+
+namespace Nette\Mail;
+
+use Nette;
+
+/**
+ * Send failed exception.
+ *
+ * @author Jan Dvořák
+ */
+class SendFailedException extends Nette\InvalidStateException
+{
+}
