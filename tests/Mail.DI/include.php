da3288e095dba65ea787aad1ebebddd44642afbd
David Grudl <david@grudl.com>
2016-05-17 12:08:23 +0200

    tests: compatible with master nette/di

--- tests/Mail.DI/include.php
+++ tests/Mail.DI/include.php
@@ -10,7 +10,7 @@ function createContainer($source, $config = NULL)
 	$loader = new Nette\DI\Config\Loader;
 	$config = $loader->load(Tester\FileMock::create($config, 'neon'));
 	$class = 'Container' . md5(lcg_value());
-	$code = $source->compile((array) $config, $class, 'Nette\DI\Container');
+	$code = @$source->compile((array) $config, $class, 'Nette\DI\Container'); // @ compatibility with di 2.4
 
 	eval($code);
 	return new $class;
