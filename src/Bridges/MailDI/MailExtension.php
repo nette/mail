374dba88ecc83fc28bc2065846bd5e4de70b43cc
David Grudl <david@grudl.com>
2020-03-25 23:08:54 +0100

    MailExtension: privateKey is not dynamic

--- src/Bridges/MailDI/MailExtension.php
+++ src/Bridges/MailDI/MailExtension.php
@@ -36,7 +36,7 @@ class MailExtension extends Nette\DI\CompilerExtension
 				Expect::structure([
 					'domain' => Expect::string()->dynamic(),
 					'selector' => Expect::string()->dynamic(),
-					'privateKey' => Expect::string()->dynamic()->required(),
+					'privateKey' => Expect::string()->required(),
 					'passPhrase' => Expect::string()->dynamic(),
 					'testMode' => Expect::bool(false)->dynamic(),
 				])->castTo('array')
